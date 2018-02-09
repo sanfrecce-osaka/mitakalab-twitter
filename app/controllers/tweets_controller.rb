a5a446475e740df8c98c0f2568dad5996fe1001e
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2018-02-09 19:33:56 +0900

    Tweetにeditとupdateを追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,8 +9,24 @@ class TweetsController < ApplicationController
   def new
   end
 
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
   def create
-    @tweet = Tweet.create(params[:tweet])
+    Tweet.create(tweet_params)
     redirect_to tweets_path
+  end
+
+  def update
+    @tweet = Tweet.find(params[:id])
+    @tweet.update(tweet_params)
+    redirect_to tweets_path
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:title, :content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :show], param: :username
-  resources :tweets, only: [:index, :show, :new, :create]
+  resources :tweets, except: :destroy
 end
