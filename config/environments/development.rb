94237ef1eff135c04de0387f373d7f124b24ea08
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2018-02-21 23:11:33 +0900

    exception_notificationを追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,9 +27,12 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -51,4 +54,12 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.middleware.use ExceptionNotification::Rack,
+    email: {
+      deliver_with: :deliver,
+      email_prefix: '[Whatever]',
+      sender_address: '"notifier" <notifier@example.com>',
+      exception_recipients: %w{exceptions@example.com}
+    }
 end
